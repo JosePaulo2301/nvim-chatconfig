bb740ea76fc00167a2b249aada95fdb95168f355
JosePaulo2301 <jose.dev2301@gmail.com>
2026-07-19 23:02:26 +0000

    Add <leader>ws shortcut to build code+2-terminals workspace layout
    Runs the same steps as manually pressing <leader>h then <leader>v,
    giving code on top and two independent terminal splits side by side
    below in one keystroke.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,3 +14,9 @@ map("t", "<C-h>", "<C-\\><C-N><C-w>h", { desc = "terminal switch window left" })
 map("t", "<C-j>", "<C-\\><C-N><C-w>j", { desc = "terminal switch window down" })
 map("t", "<C-k>", "<C-\\><C-N><C-w>k", { desc = "terminal switch window up" })
 map("t", "<C-l>", "<C-\\><C-N><C-w>l", { desc = "terminal switch window right" })
+
+-- workspace layout: code on top, 2 terminals side by side below
+map("n", "<leader>ws", function()
+  require("nvchad.term").new { pos = "sp" }
+  require("nvchad.term").new { pos = "vsp" }
+end, { desc = "terminal workspace split (code + 2 terminals)" })
